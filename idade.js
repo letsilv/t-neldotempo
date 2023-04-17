a8c798f799901bdafd6fd2b4da281acad822d1ae
letsilv <107430130+letsilv@users.noreply.github.com>
2023-04-17 15:09:00 -0300

    Add files via upload

---
+++ idade.js
@@ -0,0 +1,8 @@
+function carregaJovem() {
+    let nome = document.getElementById('nome').value;
+    let idade =  document.getElementById('Idade').value;
+
+    let idadeFutura = parseInt(idade + 40;
+
+    return document.getElementById('IdadeFutura').value = idadeFutura;
+}
